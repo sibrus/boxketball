73ef6ce91db191548198e328c0926ed4c0c82b1a
Martin Parets <martin@armyofbees.com>
2016-10-04 22:32:15 -0400

    Fixing sounds

--- audio-player/player.rb
+++ audio-player/player.rb
@@ -14,7 +14,7 @@ sound_files = {
   :basket15 => 'Basket 15.wav',
   :basket_m1 => 'Basket -1.wav',
   :basket_m3 => 'Basket -3.wav',
-  :multipler => '2X Multiplier.wav',
+  :multiplier => '2X Multiplier.wav',
   :hoop => 'Basketball Hoop.wav',
   :startup => 'Booting Up.wav',
   :game_over => 'Game Over.wav',
@@ -24,7 +24,9 @@ sound_files = {
   :sudden_death_turn => 'Sudden Death Turn Change.wav',
   :score_display => 'Score Attack - Score Display.wav',
   :score_low => 'Score Attack - Timer Low.wav',
-  :score_out => 'Score Attack - Timer Out.wav'
+  :score_out => 'Score Attack - Timer Out.wav',
+  :ready_set => 'Ready Set.wav',
+  :go => 'Go.wav'
 }
 
 puts 'Loading sounds...'
@@ -38,8 +40,8 @@ end
 puts 'Loading music...'
 
 $music = {}
-$music[:idle] = Rubygame::Music.load('./sounds/Idle Music.mp3')
-$music[:score] = Rubygame::Music.load('./sounds/Score Attack.mp3')
+$music[:idle] = Rubygame::Music.load('./sounds/Idle Music.wav')
+$music[:score] = Rubygame::Music.load('./sounds/Score Attack.wav')
 
 $redis = Redis.new(:tiemeout => 0)
 
@@ -61,33 +63,80 @@ $redis.subscribe(channels) do |on|
   on.message do |channel, msg|
     data = JSON.parse(msg)
     puts msg
-    if (channel == 'box.game.boxketball')
-      if (data['event'] == 'hitBasket')
-        case data['data']['basket']['points']
-        when 1
-          play :basket1
-        when 2
-          play :basket2
-        when 3
-          play :basket3
-        when 4
-          play :basket4
-        when 5
-          play :basket5
-        when 6
-          play :basket6
-        when 7
-          play :basket7
-        when 10
-          play :basket7
-        when -1
-          play :basket_m1
-        when -3
-          play :basket_m3
-        else
-          puts "Unknown point value: " + data['data']['basket']['points'].to_s
-        end
+    if (data['event'] == 'hitBasket')
+      case data['data']['basket']['points']
+      when 1
+        play :basket1
+      when 2
+        play :basket2
+      when 3
+        play :basket3
+      when 4
+        play :basket4
+      when 5
+        play :basket5
+      when 6
+        play :basket6
+      when 7
+        play :basket7
+      when 10
+        play :basket7
+      when 15
+        play :basket15
+      when -1
+        play :basket_m1
+      when -3
+        play :basket_m3
+      else
+        puts "Unknown point value: " + data['data']['basket']['points'].to_s
+      end
+    elsif (data['event'] == 'hitHoop')
+      play :hoop    
+    elsif (data['event'] == 'multiplierHit')
+      play :multiplier    
+    elsif (data['event'] == 'miss')
+      play :miss    
+    elsif (data['event'] == 'steal')
+      play :steal    
+    elsif (data['event'] == 'startSuddenDeath')
+      play :sudden_death
+    elsif (data['event'] == 'changeTurn' && data['gameState']['gameState'] == 'suddenDeath')
+      play :sudden_death_turn
+    elsif (data['event'] == 'preGameCountDown' && data['data']['count'] == 1)
+      play :go
+    elsif (data['event'] == 'preGameCountDown')
+      play :ready_set
+#    elsif (data['event'] == 'clockTick' && data['gameState']['players']['timeLeft'] == '11')
+#      play :score_low
+#    elsif (data['event'] == 'clockTick' && data['gameState']['players']['timeLeft'] == '0')
+#      play :score_out      
+    elsif (data['event'] == 'transition')
+      case data['data']
+      when 'idle'
+        $music[:idle].play()
+        $music[:score].stop()
+      when 'boxketball'
+        $music[:idle].stop()      
       end
     end
+    if (channel == 'box.game.boxketball')
+      if (data['event'] == 'gameOver')
+        play :game_over    
+      elsif (data['event'] == 'gameOn')
+        play :startup    
+      end
+    elsif (channel == 'box.game.scoreattack')
+      if (data['event'] == 'gameOver')
+        play :score_display    
+      elsif (data['event'] == 'gameOn')
+        $music[:score].play()
+      end    
+    end 
   end
 end
+
+#  :sudden_death => 'Sudden Death.wav',
+#  :sudden_death_turn => 'Sudden Death Turn Change.wav',
+#  :score_display => 'Score Attack - Score Display.wav',
+#  :score_low => 'Score Attack - Timer Low.wav',
+#  :score_out => 'Score Attack - Timer Out.wav'
--- audio-player/test-sound.rb
+++ audio-player/test-sound.rb
@@ -5,7 +5,7 @@ Bundler.require
 file1 = './sounds/Basket 2.wav'
 sound1 = Rubygame::Sound.load(file1)
 
-music1 = Rubygame::Music.load('./sounds/Idle Music.mp3')
+music1 = Rubygame::Music.load('./sounds/Idle Music.wav')
 music1.play()
 
 sound1.play()
